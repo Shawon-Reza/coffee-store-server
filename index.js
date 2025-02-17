038d9b95e748b086bfcf417d6b24e0f83130950f
Shawon Reza <shawon.reza.sr81@gmail.com>
2025-02-18 00:53:01 +0600

    coffe added ,post method

--- index.js
+++ index.js
@@ -5,43 +5,47 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 
-const app=express();
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
 
-
-
 const uri = `mongodb+srv://${process.env.user_name}:${process.env.user_pass}@cluster0.qgd6b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const database = client.db("Coffeelist");
+        const coffeeCollection = database.collection("coffedetails");
+
+
+        app.post("/addcoffee", async (req, res) => {
+            const coffee = req.body;
+            console.log(coffee);
+            const result = await coffeeCollection.insertOne(coffee)
+            res.send(result)
+        })
 
 
 
 
-
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -54,10 +58,10 @@ run().catch(console.dir);
 
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Coffee Store Server is running....")
 })
 
-app.listen(port,(req,res)=>{
-    console.log("Server is running on port ",port);
+app.listen(port, (req, res) => {
+    console.log("Server is running on port ", port);
 })
